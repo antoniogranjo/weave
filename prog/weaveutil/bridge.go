35ff51a077e6815b00507f95d2e06cec2cdf5037
Bryan Boreham <bjboreham@gmail.com>
2017-04-13 11:12:47 +0000

    Begun DestroyBridge

--- prog/weaveutil/bridge.go
+++ prog/weaveutil/bridge.go
@@ -46,7 +46,35 @@ func createBridge(args []string) error {
 	return err
 }
 
-// TODO: destroy-bridge
+func destroyBridge(args []string) error {
+	if len(args) != 10 {
+		cmdUsage("destroy-bridge", "<docker-bridge-name> <weave-bridge-name> <datapath-name> <mtu> <port> <mac> <no-fastdp> <no-bridged-fastdp> <proc-path> <expect-npc>")
+	}
+
+	mtu, err := strconv.Atoi(args[3])
+	if err != nil && args[3] != "" {
+		return err
+	}
+	port, err := strconv.Atoi(args[4])
+	if err != nil {
+		return err
+	}
+	config := weavenet.BridgeConfig{
+		DockerBridgeName: args[0],
+		WeaveBridgeName:  args[1],
+		DatapathName:     args[2],
+		MTU:              mtu,
+		Port:             port,
+		Mac:              args[5],
+		NoFastdp:         args[6] != "",
+		NoBridgedFastdp:  args[7] != "",
+		ExpectNPC:        args[9] == "--expect-npc",
+	}
+	procPath := args[8]
+	bridgeType, err := weavenet.CreateBridge(procPath, &config)
+	fmt.Println(bridgeType.String())
+	return err
+}
 
 func enforceAddrAsign(args []string) error {
 	if len(args) != 1 {
